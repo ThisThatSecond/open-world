a762d5d310ef902ba84dba858ad2452b71a3de45
Ehsan Ansari <eabasir@gmail.com>
2020-06-14 17:55:14 +0430

    update building procidure

--- dist/entities/project.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user.entity';
-import { Team } from './team.entity';
-export declare class Project {
-    project_id: string;
-    name: string;
-    user: User;
-    creator: User;
-    team: Team;
-}
