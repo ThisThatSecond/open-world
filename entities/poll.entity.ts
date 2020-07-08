c86d2135e176877f619ef70a02f8ae4fb2b13f7d
Ehsan Ansari <eabasir@gmail.com>
2020-07-08 09:03:35 +0430

    commit of verion 2.5.8

--- entities/poll.entity.ts
+++ entities/poll.entity.ts
@@ -20,7 +20,7 @@ import { Collection } from "./collection.entity";
 )
 @Check(
   "check_is_analytics",
-  `profile_id is not null or collection_id is not null and is_analytics_poll`
+  `(profile_id is null and collection_id is null) or is_analytics_poll not null`
 )
 @Check(
   "check_analytics_requirements",
