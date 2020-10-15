dc420a962bfed1526832b66e99ec7cc482869621
Ehsan Ansari <eabasir@gmail.com>
2020-10-15 11:06:00 +0330

    commit of verion 5.5.1

--- dist/entities/poll.entity.js
+++ dist/entities/poll.entity.js
@@ -217,7 +217,7 @@ var Poll = /** @class */ (function () {
     Poll = __decorate([
         typeorm_1.Entity("polls"),
         typeorm_1.Check("check_null_profile_id_or_collection", "(profile_id is not null and collection_id is null) or (profile_id is null and collection_id is not null)"),
-        typeorm_1.Check("check_poll_finalized", "(is_draft and finalized_at is null) or (not is_draft and finalized_at is not null)")
+        typeorm_1.Check("check_poll_finalized", "is_draft is null or (is_draft and finalized_at is null) or (not is_draft and finalized_at is not null)")
     ], Poll);
     return Poll;
 }());
--- entities/poll.entity.ts
+++ entities/poll.entity.ts
@@ -11,7 +11,7 @@ import { IGeoPoint } from "../shared/interfaces/geo_point.interface";
 
 @Entity("polls")
 @Check("check_null_profile_id_or_collection", `(profile_id is not null and collection_id is null) or (profile_id is null and collection_id is not null)`)
-@Check("check_poll_finalized", `(is_draft and finalized_at is null) or (not is_draft and finalized_at is not null)`)
+@Check("check_poll_finalized", `is_draft is null or (is_draft and finalized_at is null) or (not is_draft and finalized_at is not null)`)
 export class Poll {
   @PrimaryColumn()
   poll_id: string;
