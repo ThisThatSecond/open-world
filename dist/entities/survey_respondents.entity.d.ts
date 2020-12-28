a58e061a91fcf97c34446fc6b529c0393fc9d153
Ehsan Ansari <eabasir@gmail.com>
2020-12-28 11:10:33 +0330

    commit of verion 8.0.0-rc17

---
+++ dist/entities/survey_respondents.entity.d.ts
@@ -0,0 +1,7 @@
+import { Survey } from "./survey.entity";
+import { Profile } from "./profile.entity";
+export declare class SurveyRespondent {
+    survey: Survey;
+    profile: Profile;
+    created_at: Date;
+}
