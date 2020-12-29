cb20fabcd9bc82a411e407ad1fdb3566eb159cf8
Ehsan Ansari <eabasir@gmail.com>
2020-12-29 16:21:42 +0330

    add survey campaigns

---
+++ entities/survey_campaigns.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, ManyToOne, JoinColumn, PrimaryColumn, Index } from "typeorm";
+import { Survey } from "./survey.entity";
+import { User } from "./user.entity";
+
+@Entity("survey_campaigns")
+export class SurveyCampaign {
+  @PrimaryColumn("uuid")
+  campaign_id;
+
+  @Index()
+  @ManyToOne(() => Survey, { nullable: false })
+  @JoinColumn({
+    name: "survey_id",
+  })
+  survey: Survey;
+
+  @Column({
+    nullable: false,
+  })
+  name: string;
+
+  @ManyToOne(() => User, { nullable: false })
+  @JoinColumn({
+    name: "creator_id",
+  })
+  creator: User;
+
+  @Column({
+    type: "timestamptz",
+    default: () => "CURRENT_TIMESTAMP",
+    nullable: false,
+  })
+  created_at: Date;
+}
