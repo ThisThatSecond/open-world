c96f1c7f673523b4a4d50e3654301d2334f88d47
milad <milad@debian-BULLSEYE-live-builder-AMD64>
2021-12-21 15:31:23 +0330

    added company entity

---
+++ dist/entities/company.entity.d.ts
@@ -0,0 +1,10 @@
+export declare class Pair {
+    id: string;
+    name: string;
+    website: string;
+    logo: string;
+    company_size: string;
+    company_type: string;
+    email_domain_name: string;
+    owner_id: string;
+}
